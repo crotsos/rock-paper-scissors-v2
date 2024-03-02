784eaf36a4c73612454bed11439741f80b5eca54
Haris Rotsos <c.rotsos@lancaster.ac.uk>
2024-03-02 07:17:32 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+let set_hand_to = 0
+input.onGesture(Gesture.Shake, function () {
+    set_hand_to = randint(1, 3)
+    if (set_hand_to == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 200, 1, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+    } else if (set_hand_to == 2) {
+        basic.showLeds(`
+            # . # . #
+            # # . # #
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 200, 1, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+    } else {
+    	
+    }
+})
